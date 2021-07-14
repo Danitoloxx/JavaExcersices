764577db21e38989c883b4f270b37a3a5bebc1d7
Dani <danitoloxx@gmail.com>
2021-07-14 14:00:56 -0300

    Corregido

--- src/Exercise_6.java
+++ src/Exercise_6.java
@@ -5,7 +5,7 @@ public class Exercise_6 {
     public static void main(String[] arg){
         int max_length = 100;
 
-        for (int i = 0; i<=max_length; i++){
+        for (int i = 1; i<=max_length; i++){
             System.out.println(i);
         }
     }
